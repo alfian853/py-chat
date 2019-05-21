7f4c8a5c595475d5e39f92f205a00bda152794cb
alfian <alfian853@gmail.com>
2019-05-21 20:15:32 +0700

    add env_example.py

---
+++ py-chat/client/env_example.py
@@ -0,0 +1,5 @@
+# copy this file to env.py
+class Env:
+    server_host = '127.0.0.1'
+    server_port = 10005
+
---
+++ py-chat/server/env_example.py
@@ -0,0 +1,8 @@
+class Env:
+    server_host = '127.0.0.1'
+    server_port = 10005
+    mongodb_host = '127.0.0.1'
+    mongodb_user = '<your user>'
+    mongodb_password = '<your password>'
+    mongodb_port = 27017
+
